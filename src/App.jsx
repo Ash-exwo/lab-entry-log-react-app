799b0a4c667309aab33b67fe30ea1b42ff55dc8f
Ash-exwo <aswathygr61@gmail.com>
2026-07-14 16:27:35 +0530

    AddLab v1

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import AddLab from "./components/AddLab"
+
 function App() {
 
   return (
     <>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/add-lab" element={<AddLab/>}/>
+    </Routes>
+    </BrowserRouter>
     </>
   )
 }
